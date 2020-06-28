a5346516d2d0a32c25776169c79554ade3ecdb6f
BorisovecViktor <viktorborisovets@gmail.com>
2020-06-28 17:26:58 +0300

    Updates

---
+++ precache-manifest.fe147a7a49445f819cfeffee281a6e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb8f757efefe544b543517011adb44c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bb3991ae7d279193bc19",
+    "url": "./static/css/main.58a94d39.chunk.css"
+  },
+  {
+    "revision": "21be55d89e73ef4c5172",
+    "url": "./static/js/2.7e0fd7dd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.7e0fd7dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb3991ae7d279193bc19",
+    "url": "./static/js/main.a9194c72.chunk.js"
+  },
+  {
+    "revision": "548aac5e4720dc7e1ea8",
+    "url": "./static/js/runtime-main.9c94e0be.js"
+  }
+]);
